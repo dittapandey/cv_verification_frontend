d1c7ab24048ebe9c0a03adb3bc7ef87654373415
Yash <yashgarg.ind@gmail.com>
2022-01-16 19:20:30 +0530

    something

---
+++ src/Components/ViewRequest/RequestCard.jsx
@@ -0,0 +1,14 @@
+import { MoreVert } from '@mui/icons-material';
+import { useState } from 'react';
+import './ViewRequest.css';
+import { full } from '../../Assets/FullForm';
+
+const RequestCard = () => {
+    return ( 
+        <div className={"card "}>
+            
+        </div>
+    );
+}
+ 
+export default RequestCard;
